938f1c6eff6fabb8354be8920137bf54991a2af6
Pmacoy <42946356+Pmacoy@users.noreply.github.com>
2021-02-23 19:28:11 +0000

    definir moevimento da cobrinha

---
+++ script.js
@@ -0,0 +1,46 @@
+let canvas = document.getElementById("snake")
+let context = canvas.getContext("2d")
+let box = 32
+let snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+let direction = "right"
+
+function criarBG() {
+    context.fillStyle = "lightgreen"
+    context.fillRect(0, 0, 16 *box, 16 * box)
+}
+
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green"
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
+function iniciarJogo(){
+    criarBG()
+    criarCobrinha()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction == "right") snakeX += box
+    if(direction == "left") snakeX -= box
+    if(direction == "up") snakeY -= box
+    if(direction == "down") snakeY += box
+
+    snake.pop()
+
+    let nweHead = {
+        x: snakeX, 
+        y: snakeY
+    }
+
+    snake.unshift(nweHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100)
