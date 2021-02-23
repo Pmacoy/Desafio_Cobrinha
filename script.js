4148101cc9ed39fd8cf49ff6dbb6984680e4d2a6
Pmacoy <42946356+Pmacoy@users.noreply.github.com>
2021-02-23 20:09:51 +0000

    definir fim de jogo

--- script.js
+++ script.js
@@ -40,10 +40,18 @@ function update (event){
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo)
+            alert(" Game Over :(")
+        }
+    }
 
     criarBG()
     criarCobrinha()
